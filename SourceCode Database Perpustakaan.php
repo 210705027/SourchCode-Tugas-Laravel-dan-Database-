91dce2365d5ecd7c2c4cee9b2fb19d44d3d81ef8
210705027 <154536165+210705027@users.noreply.github.com>
2023-12-21 23:03:36 +0700

    Add files via upload

---
+++ SourceCode Database Perpustakaan.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `uas`
+ */
+
+/* `uas`.`bukus` */
+$bukus = array(
+  array('id' => '1','judul_buku' => 'Belajar Dengan Giat','pengarang_id' => '333444','tahun_terbit' => '2010','created_at' => '2023-12-21 02:36:25','updated_at' => '2023-12-21 02:36:25'),
+  array('id' => '2','judul_buku' => 'Membuat Rumah Dari Ranting','pengarang_id' => '222333','tahun_terbit' => '2030','created_at' => '2023-12-21 02:37:38','updated_at' => '2023-12-21 02:37:38'),
+  array('id' => '3','judul_buku' => 'Masa Depan Yang Gemilang','pengarang_id' => '111222','tahun_terbit' => '2025','created_at' => '2023-12-21 02:38:15','updated_at' => '2023-12-21 02:38:15')
+);
+
+/* `uas`.`failed_jobs` */
+$failed_jobs = array(
+);
+
+/* `uas`.`migrations` */
+$migrations = array(
+  array('id' => '1','migration' => '2014_10_12_000000_create_users_table','batch' => '1'),
+  array('id' => '2','migration' => '2014_10_12_100000_create_password_reset_tokens_table','batch' => '1'),
+  array('id' => '3','migration' => '2014_10_12_200000_add_two_factor_columns_to_users_table','batch' => '1'),
+  array('id' => '4','migration' => '2019_08_19_000000_create_failed_jobs_table','batch' => '1'),
+  array('id' => '5','migration' => '2019_12_14_000001_create_personal_access_tokens_table','batch' => '1'),
+  array('id' => '6','migration' => '2023_12_13_072254_create_bukus_table','batch' => '1'),
+  array('id' => '7','migration' => '2023_12_17_065047_create_pengarangs_table','batch' => '1'),
+  array('id' => '8','migration' => '2023_12_18_072512_create_sewas_table','batch' => '1'),
+  array('id' => '9','migration' => '2023_12_21_054949_create_peminjams_table','batch' => '2'),
+  array('id' => '10','migration' => '2023_12_21_063140_create_tes_table','batch' => '3')
+);
+
+/* `uas`.`password_reset_tokens` */
+$password_reset_tokens = array(
+);
+
+/* `uas`.`peminjams` */
+$peminjams = array(
+);
+
+/* `uas`.`pengarangs` */
+$pengarangs = array(
+  array('id' => '111222','nama_pengarang' => 'Wahyu Hidayat','created_at' => '2023-12-21 02:35:13','updated_at' => '2023-12-21 02:35:13'),
+  array('id' => '222333','nama_pengarang' => 'Indra Winata','created_at' => '2023-12-21 02:35:29','updated_at' => '2023-12-21 02:35:29'),
+  array('id' => '333444','nama_pengarang' => 'Mufadhal Zuhdi','created_at' => '2023-12-21 02:35:51','updated_at' => '2023-12-21 02:35:51')
+);
+
+/* `uas`.`personal_access_tokens` */
+$personal_access_tokens = array(
+);
+
+/* `uas`.`sewas` */
+$sewas = array(
+  array('id' => '1','nama_peminjam' => 'Aidil Ghufran','buku_id' => '3','tanggal_peminjaman' => '2023-12-21','tanggal_pengembalian' => '2024-01-01','created_at' => '2023-12-21 02:40:35','updated_at' => '2023-12-21 02:40:35')
+);
+
+/* `uas`.`tes` */
+$tes = array(
+);
+
+/* `uas`.`users` */
+$users = array(
+  array('id' => '1','name' => 'admin','email' => 'admin@gmail.com','email_verified_at' => NULL,'password' => '$2y$12$kney4DEyEjCMvkRLple0zOlNWM5PtgLlER.lJZE4FoXKB.EzrD0b2','two_factor_secret' => NULL,'two_factor_recovery_codes' => NULL,'two_factor_confirmed_at' => NULL,'avatar' => NULL,'remember_token' => NULL,'created_at' => NULL,'updated_at' => NULL)
+);
